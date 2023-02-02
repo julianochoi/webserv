f7433198e81a02dc19880842022e1cd708fc813c
Lucas Yuri <lucasyuri56@hotmail.com>
2023-02-02 19:21:40 -0300

    fix: main listen signals

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,16 +6,17 @@ WebServ web_serv;
 
 int	main(int argc, char **argv)
 {
+	Utils::listen_signals();
 
 	try {
 		web_serv.init(argc, argv);
-		//std::cout << web_serv << std::endl;
 		web_serv.event_loop();
 	} catch (const std::exception& e) {
 		std::cout << e.what() << std::endl;
 		return(1);
 	}
-	 
+
+	// std::cout << web_serv << std::endl;
 
 	return (0);
 }
